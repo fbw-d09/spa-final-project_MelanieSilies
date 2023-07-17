13c88cfa773be4c731eb7103acb5bd66a85447b5
Melanie Silies <melanie.silies@web.de>
2023-07-17 19:50:34 +0200

    add weather to all Layers

--- src/components/Tagesplaetze.js
+++ src/components/Tagesplaetze.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import AusflugszieleData from '../data/tagesplaetze.json';
+import React, { useEffect, useState } from 'react';
+import tagespaetzeData from '../data/tagesplaetze.json';
 import { Marker, Popup } from 'react-leaflet';
 import * as L from "leaflet";
 import dayMarker from '../assets/Marker/tagesplatz.png';
@@ -146,11 +146,135 @@ const getIcon = (key) => {
         iconSize: [50, 50],
       });
 
+      const [weatherData, setWeatherData] = useState([]);
+
+      useEffect(() => {
+          const fetchData = async () => {
+            try {
+              const promises = tagespaetzeData.features.map((feature) => {
+                const [lng, lat] = feature.geometry.coordinates;
+                return fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&hourly=temperature_2m,relativehumidity_2m,rain,showers,snowfall,cloudcover,windspeed_10m,uv_index&daily=temperature_2m_max,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max&timezone=Europe%2FBerlin&current_weather=true`)
+                  .then((response) => response.json())
+                  .catch((error) => {
+                    console.error('Error fetching weather data:', error);
+                    return null;
+                  });
+              });
+              const data = await Promise.all(promises);
+              setWeatherData(data);
+            } catch (error) {
+              console.error('Error fetching weather data:', error);
+            }
+          };
+      
+          fetchData();
+        }, []);
+
   return (
     <>
-      {AusflugszieleData.features.map((feature, index) => {
+      {tagespaetzeData.features.map((feature, index) => {
         const { Name, Website, Aktivitäten, ...properties } = feature.properties;
         const [lng, lat] = feature.geometry.coordinates;
+
+        const weather = weatherData[index];
+
+        const weatherCodeText = (code) => {
+            let weatherText
+            switch (code) {
+                case 0:
+                    weatherText = 'Klar';
+                    break;
+                case 1:
+                    weatherText = 'Überwiegend klar';
+                    break;
+                case 2:
+                    weatherText = 'teils bewölkt';
+                    break;
+                case 3:
+                    weatherText = 'bewölkt';
+                    break;
+                case 45:
+                    weatherText = 'Nebel';
+                    break;
+                case 48:
+                    weatherText = 'ablagernder Raureifnebel';
+                    break;
+
+                case 51:
+                    weatherText = 'leichter Nieselregen';
+                    break;
+                case 53:
+                    weatherText = 'Nieselregen';
+                    break;
+                case 55:
+                    weatherText = 'dichter Nieselregen';
+                    break;
+
+                case 56:
+                    weatherText = 'Gefrierender Nieselregen';
+                    break;
+                case 57:
+                    weatherText = 'starker gefrierender Nieselregen';
+                    break;
+
+                case 61:
+                    weatherText = 'leichter Regen';
+                    break;
+                case 63:
+                    weatherText = 'Regen';
+                    break;
+                case 65:
+                    weatherText = 'starker Regen';
+                    break;
+
+                case 71:
+                    weatherText = 'leichter Schneefall';
+                    break;
+                case 73:
+                    weatherText = 'Schneefall';
+                    break;
+                case 75:
+                    weatherText = 'starker Schneefall';
+                    break;
+
+                case 77:
+                    weatherText = 'Grieselschnee';
+                    break;
+
+                case 80:
+                    weatherText = 'leichte Regenschauer';
+                    break;
+                case 81:
+                    weatherText = 'Regenschauer';
+                    break;
+                case 82:
+                    weatherText = 'starke Regenschauer';
+                    break;
+
+                case 85:
+                    weatherText = 'leichte Schneeschauer';
+                    break;
+                case 86:
+                    weatherText = 'starke Schneeschauer';
+                    break;
+
+                case 95:
+                    weatherText = 'Gewitter';
+                    break;
+                case 96:
+                    weatherText = 'Gewitter mit leichtem Hagel';
+                    break;
+                case 99:
+                    weatherText = 'Gewitter mit schwerem Hagel';
+                    break;
+
+                default:
+                    weatherText = 'Das stimmt was nicht';
+                    break;
+                }
+                
+                return weatherText;
+                };
 
         return (
           <Marker key={index} position={[lat, lng]} icon={dayIcon} >
@@ -170,8 +294,15 @@ const getIcon = (key) => {
                         </p>
                       );
                     })}
-
                 </div>
+                {weather && (
+                  <div className='weather'>
+                      <b>Wetter: {weatherCodeText(weather.current_weather.weathercode)}</b>
+                      <p>Temperatur: {weather.current_weather.temperature}°C</p>
+                      <p>Windgeschwindigkeit: {weather.current_weather.windspeed} km/h</p>
+                      <p>Windrichtung: {weather.current_weather.winddirection}</p> 
+                  </div>
+                    )}
               </div>
             </Popup>
           </Marker>
